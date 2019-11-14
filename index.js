35855d0183d063dd5d8b7c5100ad1635ca94f516
Alex <alexander.fedotov.uk@gmail.com>
2019-11-14 13:41:57 +0000

    Added error handling in findImageFiles function, accepting argument to script to check a given directory.
    Signed-off-by: Alex <alexander.fedotov.uk@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk');
 const {findImageFiles} = require('./lib/search');
 
-console.log(chalk.yellow(`Welcome to optimize-images@0.0.1`));
-findImageFiles();
+console.log(chalk.yellow(`Welcome to optimize-images@v0.0.1`));
+
+// process.argv.forEach((arg) => console.log(arg));
+findImageFiles(process.argv[2]);
 
