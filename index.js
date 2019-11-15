b3e0a7d78326caae69dd5c276171a33330fbeca8
feds01 <alexander.fedotov.uk@gmail.com>
2019-11-15 10:19:31 +0000

    Added proper command line options parsing, user may specify input, output directories and verbose mode.
    By default, verbose mode is not enabled.

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
-// const chalk = require('chalk');
-// const {transformImages} = require('./lib/image');
-const {findImageFiles} = require('./lib/search');
-//
-// console.log(chalk.yellow(`Welcome to optimize-images@v0.0.1`));
-//
-// // process.argv.forEach((arg) => console.log(arg));
-// findImageFiles(process.argv[2]).then((files) => {
-//   transformImages(files, __dirname + '\\out').then((result) => {
-//     console.log(result);
-//   });
-// });
-//
+#!/usr/bin/env node
 
+const program = require('commander');
 const imageMinifier = require('imagemin');
 const mozJPEG = require('imagemin-mozjpeg');
+const {findImageFiles} = require('./lib/search');
 
-findImageFiles(process.argv[2]).then((files) => {
+// Add and parse command line options
+program.arguments('<source> [destination]')
+    .option('-i, --input <directory>', 'Input directory for images.')
+    .option('-o, --output <directory>', 'Output directory for saved images.')
+    .option('-v, --verbose', 'Program verbose mode.', false);
+
+program.parse(process.argv);
+
+// If user does not specify the source and or destination directories by flags
+// but by simple raw arguments, we can still handle this.
+if (program.input === undefined && typeof program.args[0] !== 'undefined') {
+  program.input = program.args[0];
+
+  // lets splice arguments as we already used the first argument to be
+  // assigned to the program input
+  program.args.splice(0, 1);
+}
+
+if (program.output === undefined && typeof program.args[0] !== 'undefined') {
+  program.output = program.args[0];
+
+  // do the same for the 'output' option
+  program.args.splice(0, 1);
+}
+
+// TODO: maybe use this for debug option
+// console.log(program.input, program.output, program.verbose);
+
+findImageFiles(program.input).then((files) => {
   console.log(typeof files, files);
 
   // eslint-disable-next-line max-len
-  imageMinifier(files, {destination: 'out/', plugins: [mozJPEG()]}).then(() => {
+  imageMinifier(files, {
+    destination: program.output,
+    plugins: [mozJPEG()],
+  }).then(() => {
     console.log('Images optimized');
   });
 });
