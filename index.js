43546f8251dc520e52747cf1c3b3a3446306f27d
Alex <alexander.fedotov.uk@gmail.com>
2019-11-15 09:04:26 +0000

    Added transformation function for JPEG images and findImageFiles returns results.
    Signed-off-by: Alex <alexander.fedotov.uk@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const chalk = require('chalk');
+// const chalk = require('chalk');
+// const {transformImages} = require('./lib/image');
 const {findImageFiles} = require('./lib/search');
+//
+// console.log(chalk.yellow(`Welcome to optimize-images@v0.0.1`));
+//
+// // process.argv.forEach((arg) => console.log(arg));
+// findImageFiles(process.argv[2]).then((files) => {
+//   transformImages(files, __dirname + '\\out').then((result) => {
+//     console.log(result);
+//   });
+// });
+//
 
-console.log(chalk.yellow(`Welcome to optimize-images@v0.0.1`));
+const imageMinifier = require('imagemin');
+const mozJPEG = require('imagemin-mozjpeg');
 
-// process.argv.forEach((arg) => console.log(arg));
-findImageFiles(process.argv[2]);
+findImageFiles(process.argv[2]).then((files) => {
+  console.log(typeof files, files);
 
+  // eslint-disable-next-line max-len
+  imageMinifier(files, {destination: 'out/', plugins: [mozJPEG()]}).then(() => {
+    console.log('Images optimized');
+  });
+});
