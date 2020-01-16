1679f5bc78dd4090107b7e0aab449b343b300ece
Alex <alexander.fedotov.uk@gmail.com>
2020-01-16 15:43:45 +0000

    First working version of script!
    lib/image:
    - implemented function to calculate difference between source file and final file.
    - re-worked transformImages function to work consistently.
    
    lib/search:
    - re-worked findImageFiles to properly utilise fs.readdirSync method properly.
    - implemented verbosity within function.
    
    index:
    - implemented cli interface.
    
    Signed-off-by: Alex <alexander.fedotov.uk@gmail.com>

--- index.js
+++ index.js
@@ -1,25 +1,73 @@
-// const chalk = require('chalk');
-// const {transformImages} = require('./lib/image');
+const prompt = require('prompt');
+const program = require('commander');
+const chalk = require('chalk');
+const path = require('path');
+
 const {findImageFiles} = require('./lib/search');
-//
-// console.log(chalk.yellow(`Welcome to optimize-images@v0.0.1`));
-//
-// // process.argv.forEach((arg) => console.log(arg));
-// findImageFiles(process.argv[2]).then((files) => {
-//   transformImages(files, __dirname + '\\out').then((result) => {
-//     console.log(result);
-//   });
-// });
-//
+const {transformImages, fileSizeDifference} = require('./lib/image');
 
-const imageMinifier = require('imagemin');
-const mozJPEG = require('imagemin-mozjpeg');
+program.name('optimise-image');
+program.version('v0.0.1');
 
-findImageFiles(process.argv[2]).then((files) => {
-  console.log(typeof files, files);
+program.arguments('<source> <destination>')
+    .option('-i, --input <directory>', 'Input directory for images.')
+    .option('-o, --output <directory>', 'Output directory for saved images.')
+    .option('-v, --verbose', 'Program verbose mode.', false);
 
-  // eslint-disable-next-line max-len
-  imageMinifier(files, {destination: 'out/', plugins: [mozJPEG()]}).then(() => {
-    console.log('Images optimized');
-  });
+program.parse(process.argv);
+
+// If user does not specify the sources and or destination directories by flags
+// but by raw arguments, we can still handle this.
+if (program.input === undefined && typeof program.args[0] !== 'undefined') {
+  program.input = path.resolve(program.args[0]);
+
+  // lets splice the arguments as we already used the first argument to be
+  // assigned to the program input
+  program.args.splice(0, 1);
+}
+
+if (program.output === undefined && typeof program.args[0] !== 'undefined') {
+  program.output = path.resolve(program.args[0]);
+
+  // do the same for the 'output' option
+  program.args.splice(0, 1);
+}
+
+// If verbose not specified, set it to 'false' by default
+if (program.verbose === undefined) {
+  program.verbose = false;
+}
+
+// eslint-disable-next-line max-len
+const images = findImageFiles(path.resolve(program.input), program.verbose);
+
+// Start the prompt
+prompt.start();
+// Get two properties from the user: username and email
+prompt.get([{
+  name: 'confirmation',
+  description: chalk.green('Optimise images [y/N] ?'),
+  type: 'string',
+  message: 'Confirmation must be a "y" or "n"',
+  required: true,
+  conform: (value) => {
+    return ['y', 'n'].includes(value.toLowerCase());
+  },
+}], (err, result) => {
+  if (result.confirmation.toLowerCase() === 'y') {
+    transformImages(images, program.output).then((k) => {
+      let savedSpace = 0;
+
+      k.forEach((item) => {
+        savedSpace += fileSizeDifference(item.sourcePath, item.destinationPath);
+      });
+
+      // Convert size into megabytes and round to two decimal figures.
+      savedSpace = (savedSpace/ 1E6).toFixed(2);
+
+      if (program.verbose) console.log(`total space saved from optimisation ${chalk.green.bold(savedSpace)}Mb.`);
+
+      console.log('optimised image files were written to ' + program.output);
+    });
+  }
 });
